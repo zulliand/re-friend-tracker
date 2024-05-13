e1e0a718b8927cd0e31524d02a0d022d990adfa1
Andres Zulliger <zulliand@students.zhaw.ch>
2024-05-13 17:11:22 +0000

    V6

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -1,5 +1,9 @@
 export class GuiModel {
 
+    /* HINWEIS: Texte sind in der Datei ../example-translation-service.ts definiert.
+    Erscheinen Texte in {}, so kann die Übersetzung in example-translation-service.ts definiert werden
+    */ 
+
     private _guiModel = {
         "application": {
             "title": "Andres Zulliger Requirements Engineering Friend Tracker (FS2024)",
